e314c8e44c3a60d995db1bb2a3819e056a8d9c08
a <a>
2021-11-11 15:32:27 -0400

    Add url name and version calls. Removed old get pages functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
 use std::env;
-use std::panic;
-use std::time;
-use std::net::{Shutdown, TcpStream, SocketAddr};
 use std::path::Path;
 use std::process::Command;
 use std::str;
@@ -12,8 +9,9 @@ extern crate sciter;
 use sciter::dispatch_script_call;
 use sciter::Value;
 
-const VERSION: &str = "0.3.0"; // Note: And cargo.toml
-const NAME: &str = "Transmitic In Development Alpha";
+const VERSION: &str = "0.10.0"; // Note: And cargo.toml
+const NAME: &str = "Transmitic Beta";
+const URL: &str = "https://transmitic.io";
 
 struct Handler {
 }
@@ -80,112 +78,59 @@ impl Handler {
 	}
 
 
-	fn get_name(&self) -> Value {
+	fn get_app_display_name(&self) -> Value {
 		Value::from(NAME)
 	}
 
-	fn get_version(&self) -> Value {
-		Value::from(VERSION)
+	fn get_app_display_version(&self) -> Value {
+		Value::from(format!("v{}", VERSION))
 	}
 
-	fn get_page_main_bytes(&self) -> Vec<u8> {
-		let html = include_bytes!("main.htm");
-		//let html_bytes = html_final.as_bytes().to_vec();
-		return html.to_vec();
-	}
-
-	fn get_page_about(&self) -> Value {
-		let page_bytes = include_bytes!("about.htm");
-		let page_str = self.finalize_htm_page(page_bytes);
-		Value::from(page_str)
-	}
-
-	fn get_page_downloads(&self) -> Value {
-		let page_bytes = include_bytes!("downloads.htm");
-		let page_str = self.finalize_htm_page(page_bytes);
-		Value::from(page_str)
-	}
-
-	fn get_page_my_sharing(&self) -> Value {
-		let page_bytes = include_bytes!("my_sharing.htm");
-		let page_str = self.finalize_htm_page(page_bytes);
-		Value::from(page_str)
-	}
-
-	fn get_page_shared_with_me(&self) -> Value {
-		let page_bytes = include_bytes!("shared_with_me.htm");
-		let page_str = self.finalize_htm_page(page_bytes);
-		Value::from(page_str)
-	}
-
-	fn get_page_welcome(&self) -> Value {
-		let page_bytes = include_bytes!("welcome.htm");
-		let page_str = self.finalize_htm_page(page_bytes);
-		Value::from(page_str)
-	}
-
-	fn get_page_my_id(&self) -> Value {
-		let page_bytes = include_bytes!("my_id.htm");
-		let page_str = self.finalize_htm_page(page_bytes);
-		Value::from(page_str)
-	}
-
-	fn get_page_users(&self) -> Value {
-		let page_bytes = include_bytes!("users.htm");
-		let page_str = self.finalize_htm_page(page_bytes);
-		Value::from(page_str)
-	}
-
-	/// This is hack to get around loading CSS on htm pages with sciter
-	/// 1. I can't get a relative to path to load css in a frame page
-	/// 2. SC_LOAD_DATA works for loading files, but using a frame causes a crash
-	/// I don't have any more time to deal with this, so I inject the CSS into the page
-	fn inject_css_into_page(&self, page: &str) -> String {
-		let bytes = include_bytes!("style.css");
-		let css_str = str::from_utf8(bytes).unwrap();
-		let new_page = page.replace("/* AUTO CSS INJECTION */", css_str);
-		return new_page;
-	}
-
-	fn finalize_htm_page(&self, page_bytes: &[u8]) -> String {
-		let page_str = str::from_utf8(page_bytes).unwrap();
-		let page_str = self.inject_css_into_page(page_str);
-		return page_str;
-	}
+    fn get_app_url(&self) -> Value {
+        Value::from(URL)
+    }
 
 	fn get_local_ip(&self) -> Value {
 		// TODO
 		Value::from("192.168.X.X")
 	}
 
+    fn get_public_id(&self) -> Value {
+        Value::from("PUBLICHERE")
+    }
+
 }
 
 impl sciter::EventHandler for Handler {
 	dispatch_script_call! {
-		fn open_a_download(Value);
 		fn fake_click(Value);
+
+        fn get_app_display_name();
+        fn get_app_display_version();
+        fn get_app_url();
+        
+        fn get_downloads_in_progress();
 		fn get_local_ip();
-		fn get_downloads_in_progress();
-		fn get_name();
-		fn get_page_about();
-		fn get_page_downloads();
-		fn get_page_my_id();
-		fn get_page_my_sharing();
-		fn get_page_shared_with_me();
-		fn get_page_welcome();
-		fn get_page_users();
-		fn get_version();
+        fn get_public_id();
+
+        fn open_a_download(Value);
 	}
 }
 
 fn main() {
-
+    println!("{} v{}", NAME, VERSION);
+    println!("{}", URL);
 	let args: Vec<String> = env::args().collect();
-	println!("CLI args");
+	println!("cli args");
 	println!("{:?}\n", args);
 
-	println!("Current Working Dir: {:?}", env::current_dir().unwrap());
-	println!("Transmitic Path: {:?}", env::current_exe().unwrap());
+	let mut sciter_path = env::current_dir().unwrap();
+    sciter_path.push("transmitic\\src\\main.htm");
+    let sciter_string = format!("file://{}", sciter_path.to_string_lossy());
+
+    println!("Current Working Dir: '{:?}'", env::current_dir().unwrap());
+	println!("Transmitic Path: '{:?}'", env::current_exe().unwrap());
+    println!("Sciter path: '{}'", sciter_string);
 
 	sciter::set_options(sciter::RuntimeOptions::ScriptFeatures(
 		sciter::SCRIPT_RUNTIME_FEATURES::ALLOW_SYSINFO as u8
@@ -196,7 +141,6 @@ fn main() {
 
 	let mut handler = Handler {
 	};
-	let html_main_bytes = handler.get_page_main_bytes();
 
 	let mut frame = sciter::Window::new();
 	frame.event_handler(handler);
@@ -207,13 +151,6 @@ fn main() {
 			.unwrap();
 	}
 
-	//frame.load_html(&html_main_bytes, Some("example://app/main.htm"));
-	let mut path = env::current_dir().unwrap();
-	//path.pop();
-	path.push("transmitic\\src\\main.htm");
-
-	let fullp = format!("file://{}", path.to_string_lossy());
-	println!("fullp {:?}", fullp);
-	frame.load_file(&fullp);
+	frame.load_file(&sciter_string);
 	frame.run_app();
 }
