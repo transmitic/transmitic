fdfd3831edcfc480c688226acdba50f0ea5f98a5
Andrew Shay <andrew.shay.git@protonmail.com>
2023-08-19 21:12:54 -0400

    Linux build script

--- build/build.py
+++ build/build.py
@@ -4,9 +4,17 @@ import shutil
 import subprocess
 from datetime import date
 
+system = platform.system().lower()
+if system == "darwin":
+    system = "mac"
+
+is_win = system == "windows"
+is_mac = system == "mac"
+is_linux = system == "linux"
+
 website = "https://transmitic.net"
 BINARY_NAME = "transmitic"
-if platform.system() == "Windows":
+if is_win:
     BINARY_NAME = "transmitic.exe"
 
 print("#### BUILD TRANSMITIC ####")
@@ -25,18 +33,22 @@ os.chdir(workspace_path)
 print(f"workspace path: {workspace_path}")
 print(f"cwd: {os.getcwd()}")
 
-sciter_dir = os.path.join(os.path.dirname(workspace_path), "sciter-js-sdk")
-sciter_dll_name = "sciter.dll"
-sciter_dll_path = os.path.join(sciter_dir, "bin", "windows", "x64", sciter_dll_name)
+if is_win:
+    sciter_dir = os.path.join(os.path.dirname(workspace_path), "sciter-js-sdk")
+    sciter_dll_name = "sciter.dll"
+    sciter_dll_path = os.path.join(sciter_dir, "bin", "windows", "x64", sciter_dll_name)
+elif is_linux:
+    sciter_dll_name = "libsciter-gtk.so"
+    sciter_dll_path = os.path.join(workspace_path, sciter_dll_name)
 
 # Check prereqs
-if platform.system() == "Windows":
+if is_win:
     rc_edit_path = os.path.join(workspace_path, "more", "rcedit-x64.exe")
     if not os.path.exists(rc_edit_path):
         raise Exception(f"rcedit not found at {rc_edit_path}")
 
-manifest_path = os.path.join(HERE, "transmitic.exe.manifest")
-print(f"manifest path: {manifest_path}")
+    manifest_path = os.path.join(HERE, "transmitic.exe.manifest")
+    print(f"manifest path: {manifest_path}")
 
 print("\n\n")
 # Find cargo version
@@ -64,7 +76,7 @@ new_release_root_dir = new_release_dir = os.path.join(
     workspace_path, "releases", f"a_staging_transmitic_v{version}"
 )
 new_release_dir = os.path.join(
-    new_release_root_dir, "windows"
+    new_release_root_dir, system
 )
 if os.path.exists(new_release_dir):
     shutil.rmtree(new_release_dir)
@@ -138,87 +150,95 @@ def run_rc_edit(file_path):
     print(result)
 
 
-if platform.system() == "Windows":
+if is_win:
     run_rc_edit(transmitic_exe_path)
 
 # -- Portable dir
-portable_dir = os.path.join(new_release_dir, f"transmitic_v{version}_windows_portable")
-zip_path = os.path.join(new_release_dir, f"transmitic_v{version}_windows_portable.zip")
-sub_portable_dir = os.path.join(portable_dir, f"transmitic_v{version}_windows")
+portable_dir = os.path.join(new_release_dir, f"transmitic_v{version}_{system}_portable")
+zip_path = os.path.join(new_release_dir, f"transmitic_v{version}_{system}_portable.zip")
+sub_portable_dir = os.path.join(portable_dir, f"transmitic_v{version}_{system}")
 os.makedirs(portable_dir, exist_ok=False)
 shutil.copytree(res_path, os.path.join(sub_portable_dir, 'res'), dirs_exist_ok=True)
 shutil.copy2(os.path.join(new_release_dir, sciter_dll_name), os.path.join(sub_portable_dir, sciter_dll_name))
 shutil.copy2(os.path.join(new_release_dir, BINARY_NAME), os.path.join(sub_portable_dir, BINARY_NAME))
 os.chdir(new_release_dir)
-shutil.make_archive(f"transmitic_v{version}_windows_portable", 'zip', root_dir=portable_dir)
+shutil.make_archive(f"transmitic_v{version}_{system}_portable", 'zip', root_dir=portable_dir)
+if is_linux:
+    os.chdir(portable_dir)
+    tar_file_name = f"transmitic_v{version}_{system}_portable.tar.gz"
+    cmd = f"tar cfz {tar_file_name} transmitic_v{version}_{system}"
+    res = subprocess.run(cmd, check=True, shell=True)
+    print(res)
+    shutil.move(tar_file_name, new_release_dir)
 os.chdir(workspace_path)
 
-# -- Installer MSI
-shutil.copy2(os.path.join(HERE, 'license.rtf'), new_release_dir)
-shutil.copy2(os.path.join(HERE, 'WixUIBannerBmp.png'), new_release_dir)
-shutil.copy2(os.path.join(HERE, 'WixUIDialogBmp.png'), new_release_dir)
-shutil.copy2(os.path.join(HERE, 'transmitic_installed.json'), new_release_dir)
+if is_win:
+    # -- Installer MSI
+    shutil.copy2(os.path.join(HERE, 'license.rtf'), new_release_dir)
+    shutil.copy2(os.path.join(HERE, 'WixUIBannerBmp.png'), new_release_dir)
+    shutil.copy2(os.path.join(HERE, 'WixUIDialogBmp.png'), new_release_dir)
+    shutil.copy2(os.path.join(HERE, 'transmitic_installed.json'), new_release_dir)
 
-# Create MSI xml
-with open(os.path.join(HERE, "transmitic_msi.wxs"), 'r', encoding='utf=8') as f:
-    text = f.read()
-res_files = os.listdir(res_path)
-res_features = ""
-res_components = ""
-all_component_ids = []
-for f in res_files:
-    component_id = f.replace("-", "_")  # - is not valid in wix
-    if component_id in all_component_ids:
-        raise Exception(f"ComponentID already exists: {component_id}")
-    all_component_ids.append(component_id)
+    # Create MSI xml
+    with open(os.path.join(HERE, "transmitic_msi.wxs"), 'r', encoding='utf=8') as f:
+        text = f.read()
+    res_files = os.listdir(res_path)
+    res_features = ""
+    res_components = ""
+    all_component_ids = []
+    for f in res_files:
+        component_id = f.replace("-", "_")  # - is not valid in wix
+        if component_id in all_component_ids:
+            raise Exception(f"ComponentID already exists: {component_id}")
+        all_component_ids.append(component_id)
 
-    res_features += f'			<ComponentRef Id="{component_id}"/>\n'
-    res_components += f"""                    <Component Id="{component_id}">
-                          <File Id="{component_id}" KeyPath="yes" Source="res\\{f}">
-                          </File>
-                    </Component>
-"""
-exit_text = f"{website}"
-text = text.format(RES_FEATURES=res_features, RES_COMPONENTS=res_components, VERSION=version, EXIT_TEXT=exit_text)
+        res_features += f'			<ComponentRef Id="{component_id}"/>\n'
+        res_components += f"""                    <Component Id="{component_id}">
+                              <File Id="{component_id}" KeyPath="yes" Source="res\\{f}">
+                              </File>
+                        </Component>
+    """
+    exit_text = f"{website}"
+    text = text.format(RES_FEATURES=res_features, RES_COMPONENTS=res_components, VERSION=version, EXIT_TEXT=exit_text)
 
-msi_xml_path = os.path.join(new_release_dir, f'transmitic_v{version}_windows.wxs')
-with open(msi_xml_path, 'w', encoding='utf-8') as f:
-    f.write(text)
+    msi_xml_path = os.path.join(new_release_dir, f'transmitic_v{version}_windows.wxs')
+    with open(msi_xml_path, 'w', encoding='utf-8') as f:
+        f.write(text)
 
-# Build msi
-os.chdir(new_release_dir)
-command = f"wix build {msi_xml_path} -ext WixToolset.UI.wixext"
-res = subprocess.run(command, check=True, shell=True)
-print(res)
-os.chdir(workspace_path)
-output_msi_path = os.path.join(new_release_dir, f'transmitic_v{version}_windows.msi')
+    # Build msi
+    os.chdir(new_release_dir)
+    command = f"wix build {msi_xml_path} -ext WixToolset.UI.wixext"
+    res = subprocess.run(command, check=True, shell=True)
+    print(res)
+    os.chdir(workspace_path)
+    output_msi_path = os.path.join(new_release_dir, f'transmitic_v{version}_windows.msi')
 
-# -- Burn Bundle
+    # -- Burn Bundle
 
-# Create Burn xml
-with open(os.path.join(HERE, "transmitic_installer.wxs"), 'r', encoding='utf=8') as f:
-    text = f.read()
-vc_redist_path = os.path.join(workspace_path, "vc_redist.x64.exe")
-text = text.format(VERSION=version, WEBSITE=website, MSI_FILE=output_msi_path, VC_REDIST_FILE=vc_redist_path)
+    # Create Burn xml
+    with open(os.path.join(HERE, "transmitic_installer.wxs"), 'r', encoding='utf=8') as f:
+        text = f.read()
+    vc_redist_path = os.path.join(workspace_path, "vc_redist.x64.exe")
+    text = text.format(VERSION=version, WEBSITE=website, MSI_FILE=output_msi_path, VC_REDIST_FILE=vc_redist_path)
 
-burn_xml_path = os.path.join(new_release_dir, f'transmitic_v{version}_windows_installer.wxs')
-output_installer_path = os.path.join(new_release_dir, f'transmitic_v{version}_windows_installer.exe')
-with open(burn_xml_path, 'w', encoding='utf-8') as f:
-    f.write(text)
+    burn_xml_path = os.path.join(new_release_dir, f'transmitic_v{version}_windows_installer.wxs')
+    output_installer_path = os.path.join(new_release_dir, f'transmitic_v{version}_windows_installer.exe')
+    with open(burn_xml_path, 'w', encoding='utf-8') as f:
+        f.write(text)
 
-# Build burn
-os.chdir(new_release_dir)
+    # Build burn
+    os.chdir(new_release_dir)
 
-res = subprocess.run("wix extension add WixToolset.Util.wixext", check=True, shell=True)
-print(res)
+    res = subprocess.run("wix extension add WixToolset.Util.wixext", check=True, shell=True)
+    print(res)
 
-res = subprocess.run("wix extension add WixToolset.Bal.wixext", check=True, shell=True)
-print(res)
+    res = subprocess.run("wix extension add WixToolset.Bal.wixext", check=True, shell=True)
+    print(res)
 
-command = f"wix build {burn_xml_path} -ext WixToolset.Bal.wixext -ext WixToolset.UI.wixext"
-res = subprocess.run(command, check=True, shell=True)
-print(res)
-os.chdir(workspace_path)
+    command = f"wix build {burn_xml_path} -ext WixToolset.Bal.wixext -ext WixToolset.UI.wixext"
+    res = subprocess.run(command, check=True, shell=True)
+    print(res)
+    os.chdir(workspace_path)
 
 # -- Copy Cargo.lock
 shutil.copy2(os.path.join(workspace_path, "Cargo.lock"), new_release_dir)
